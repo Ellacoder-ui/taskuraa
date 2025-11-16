bdcbb4c8f90b42f185573cb7e931fca624a575b2
Kent Gabe <kgabe46585@liceo.edu.ph>
2025-11-16 18:03:32 +0800

    task_user

--- src/main/java/com/taskura/taskuraa/HelloController.java
+++ src/main/java/com/taskura/taskuraa/HelloController.java
@@ -1,14 +1,55 @@
 package com.taskura.taskuraa;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController{
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button homeButton;
+    @FXML
+    private Button calendarButton;
+    @FXML
+    private Button tasksButton;
+    @FXML
+    private TextField searchTasksField;
+    @FXML
+    private Button newButton;
+    @FXML
+    private VBox taskListContainer;
+    @FXML
+    private Label activityTitleLabel;
+    @FXML
+    private Label deadlineLabel;
+
+    public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("HelloController initialized successfully.");
+    }
+
+
+    @FXML
+    protected void onHomeButtonClicked() {
+        System.out.println("Home button clicked.");
+    }
+
+    @FXML
+    protected void onTasksButtonClicked() {
+        System.out.println("Tasks button clicked.");
+    }
+
+    @FXML
+    protected void onNewTaskClicked() {
+        System.out.println("'+ New' button clicked.");
+    }
+
+    @FXML
+    protected void onAddMoreTasksClicked() {
+        System.out.println("'+ Add more tasks' button clicked.");
     }
 }
